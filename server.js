be183d243ec51739cc7b7ec7f5a99055a35aa00f
juanmaparrado <juanmanuel.parrado.fp@iescampanillas.com>
2023-02-23 09:31:27 +0100

    update all

--- server.js
+++ server.js
@@ -9,7 +9,6 @@ const server = http.createServer((req,res) => {
     res.setHeader('Content-Type',"text/plain");
     res.end(message);
 });
- 
 server.listen(port, hostname, () => {
     console.log(`Servidor corriendo en http://${hostname()}:${port}/`);
 });
